af04be923cc55cea6cbb4edec48ded756d5401a7
FAHADPN <fahadpuzhakkaraillath@gmail.com>
2024-10-08 17:18:04 +0530

    Created api to accept recording uri

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import recordingViewSet
+from .views import recordingViewSet, recording
 
 router = DefaultRouter()
 router.register(r"recording", recordingViewSet)
 
 urlpatterns = [
+    path("recording/", recording.as_view(), name="recording"),
     path("", include(router.urls)),
 ]
--- app/views.py
+++ app/views.py
@@ -2,7 +2,17 @@ from rest_framework import serializers
 from .serializers import recordingSerializer
 from .models import recording
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from ehr.models import Patient
 
 class recordingViewSet(viewsets.ModelViewSet):
     queryset = recording.objects.all()
     serializer_class = recordingSerializer
+
+api_view(['POST'])
+def recording(request):
+    data = request.data
+    patient = Patient.objects.get(id=1)
+    recording.objects.create(patient=patient, recording=data['recording'])
+    return Response({'status': 'recording created'})
