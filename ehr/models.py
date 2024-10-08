ec2d3e19eccca6473896a77e56ae84338091f9d6
FAHADPN <fahadpuzhakkaraillath@gmail.com>
2024-10-08 14:57:51 +0530

    unique together

--- ehr/models.py
+++ ehr/models.py
@@ -6,3 +6,9 @@ class Patient(models.Model):
     name = models.CharField(max_length=200)
     age = models.IntegerField()
     phone = models.CharField(max_length=25, blank=True)
+
+    class Meta:
+        unique_together = ('name', 'age', 'phone')  # Ensure the co mbination is unique
+
+    def __str__(self):
+        return f"{self.name}, Age: {self.age}, Phone: {self.phone}"
